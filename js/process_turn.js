01b1060327a1cf7122e299bba51465e91ee923d8
InnkeeperNL <hotfmn@gmail.com>
2025-02-01 06:28:01 +0100

    Update process_turn.js

--- js/process_turn.js
+++ js/process_turn.js
@@ -6924,12 +6924,20 @@ function play_unit_card(side, card_id, origin_id, forced_play, origin_unit){
 			$('.battle_container').append(parsed_unit);
 		},total_timeout);
 		timeout_key ++;
+		var temp_hand_slot = battle_info['deck_' + side][origin_id]['hand_slot'] + 0;
+		var temp_side = side + 0;
+		var temp_hand_slot_id = battle_info['deck_' + side][origin_id]['hand_slot_id'] + 0;
+
 		all_timeouts[timeout_key] = setTimeout(function(){
 				$('.unit_id_' + next_combat_unit_id).removeClass('hand_card');
 				$('.unit_id_' + next_combat_unit_id).removeClass('fake_hand_slot_' + origin_hand_slot);
 				$('.unit_id_' + next_combat_unit_id).removeClass(temp_slot_2);
 				$('.unit_id_' + next_combat_unit_id).addClass('slot_' + temp_slot);
+				$('.hand_slot_' + temp_hand_slot + '.side_' + temp_side + '.hand_slot_id_' + temp_hand_slot_id).each(function(){
+					this.remove();	
+				});
 			},total_timeout + 150);
+		battle_info['deck_' + side][origin_id]['status'] = 'in_play';
 		check_visible_skills(next_combat_unit_id);
 		update_passive_effects(next_combat_unit_id);
 		total_timeout += 1000 * battle_speed;
@@ -6967,6 +6975,7 @@ function play_unit_card(side, card_id, origin_id, forced_play, origin_unit){
 				}
 			});
 		}
+		if(battle_info['deck_' + side][origin_id]['status'] != 'in_play'){played_card = false;}
 		
 	}
 	return played_card;
