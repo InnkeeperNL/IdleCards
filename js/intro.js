bf04b2799bedc27d5d37ad423eba41e2cd110f20
InnkeeperNL <hotfmn@gmail.com>
2025-10-26 07:52:53 +0100

    intro

--- js/intro.js
+++ js/intro.js
@@ -22,7 +22,9 @@ function show_next_step(){
 	var current_step = intro_steps[intro_step];
 	var current_step_id = intro_step + 0;
 	var parsed_step = '';
-	parsed_step += '<div class="intro_step step_' + current_step_id + '" style="';
+	var step_is_text = '';
+	if(current_step['text'] != undefined){step_is_text = 'intro_text';}
+	parsed_step += '<div class="intro_step step_' + current_step_id + ' ' + step_is_text + '" style="';
 		if(current_step['text_size'] != undefined){parsed_step += 'font-size:' + current_step['text_size'] + ';';}
 		if(current_step['text_align'] != undefined){parsed_step += 'text-align:' + current_step['text_align'] + ';';}
 		if(current_step['start_location'] != undefined){
@@ -30,11 +32,13 @@ function show_next_step(){
 				parsed_step += location_key + ':' + location_info + ';';
 			});
 		}		
-		if(current_step['image'] != undefined && current_step['image'] != 'none'){parsed_step += 'background-image:url(' + current_step['image'] + ');';}
+		if(current_step['image'] != undefined && current_step['image'] != 'none'){parsed_step += 'background-image:url(images/' + current_step['image'] + ');';}
 	parsed_step += '">';
 	if(current_step['text'] != undefined)
 	{
+		parsed_step += '<div class="intro_text">';
 		parsed_step += current_step['text'];
+		parsed_step += '</div>';
 	}
 	parsed_step += '</div>';
 
@@ -81,477 +85,236 @@ function skip_intro(){
 
 var intro_steps = {
 	0:{
-		timeout: 	0,
-		time_on_screen: 2000,
-		text: 		'Long ago...',
-		text_size: 	'10vw',
-		image: 		'none',
+		time_on_screen: 	3000,
+		text: 				'The Great War is over...',
 		start_location: 	{
 							left: 	'10%',
 							top: 	'30%'
 		},
 		end_location: 		{
 							left: 	'15%',
-							top: 	'30%'
 		},
 	},
 	1:{
-		timeout: 			2500,
-		time_on_screen: 	3000,
-		image: 				'images/cards/butterfly-2049567_640.jpg',
-		opacity: 			0.3,
-		start_location: 	{
-							right: 	'0%',
-							top: 	'0%',
-							left: 	'0%',
-							bottom: '0%',
-		},
-		end_location: 		{
-							right: 	'-5%',
-							top: 	'-5%',
-							left: 	'-0%',
-							bottom: '-0%',
-		},
+		time_on_screen: 	4000,
+		image: 				'corpse_beach',
+		image_h: 			'move_left',
+		image_v: 			'move_down',
 	},
 	2:{
-		timeout: 	2500,
-		time_on_screen: 2000,
-		text: 		'There was peace in the world',
-		text_size: 	'5vw',
-		text_align: 'right',
-		image: 		'none',
+		text: 				'The bodies of fathers, sons and brothers are scattered across the land.',
 		start_location: 	{
 							right: 	'10%',
-							top: 	'60%',
-							left:   '-100%',
+							bottom: '40%'
 		},
 		end_location: 		{
 							right: 	'15%',
-							top: 	'60%'
 		},
 	},
 	3:{
-		timeout: 			6500,
-		time_on_screen: 	3000,
-		image: 				'images/cards/fantasy-3366526_640.jpg',
-		opacity: 			0.3,
-		start_location: 	{
-							right: 	'0%',
-							top: 	'0%',
-							left: 	'0%',
-							bottom: '-120%',
-		},
-		end_location: 		{
-							right: 	'-5%',
-							top: 	'-0%',
-							left: 	'-5%',
-							bottom: '-140%',
-		},
+		time_on_screen: 	4000,
+		image: 				'blood_witch',
+		image_v: 			'move_down',
 	},
 	4:{
-		timeout: 	6000,
-		time_on_screen: 3000,
-		text: 		'Then man arrived',
-		text_size: 	'5vw',
-		text_align: 'left',
-		image: 		'none',
+		text: 				'Those that survived mourn their loss.',
 		start_location: 	{
 							left: 	'10%',
-							bottom: '20%',
-							right:   '-100%',
+							bottom: '10%'
 		},
 		end_location: 		{
 							left: 	'15%',
-							bottom: '20%'
 		},
 	},
 	5:{
-		timeout: 			10000,
-		time_on_screen: 	3500,
-		image: 				'images/cards/confrontation-930744_640.jpg',
-		opacity: 			0.3,
-		start_location: 	{
-							right: 	'-10%',
-							top: 	'-0%',
-							left: 	'-0%',
-							bottom: '-100%',
-		},
-		end_location: 		{
-							right: 	'-0%',
-							top: 	'-0%',
-							left: 	'-10%',
-							bottom: '-100%',
-		},
+		time_on_screen: 	5000,
+		image: 				'haunted_forest',
+		image_v: 			'move_up',
 	},
 	6:{
-		timeout: 	        10000,
-		time_on_screen:     2500,
-		text: 		'Aggressive by nature...',
-		text_size: 	'5vw',
-		text_align: 'right',
-		image: 		'none',
+		text: 				'The gods, either slain or tired, no longer roam the world.',
 		start_location: 	{
-							left: 	'-110%',
-							top:    '20%',
-							right:   '10%',
+							right: 	'10%',
+							top: '10%'
 		},
 		end_location: 		{
 							right: 	'15%',
-							top:    '20%'
 		},
 	},
 	7:{
-		timeout: 			13000,
-		time_on_screen: 	5500,
-		image: 				'images/cards/barbarian-4616094_640.jpg',
-		opacity: 			0.3,
-		start_location: 	{
-							right: 	'-0%',
-							top: 	'-20%',
-							left: 	'-10%',
-							bottom: '-80%',
-		},
-		end_location: 		{
-							right: 	'-0%',
-							top: 	'-0%',
-							left: 	'-0%',
-							bottom: '-150%',
-		},
+		time_on_screen: 	6000,
+		image: 				'freya',
+		image_h: 			'move_right',
+		image_v: 			'move_up',
 	},
 	8:{
-		timeout: 	        13000,
-		time_on_screen:     2500,
-		text: 		'men started fighting<br/>amongst themselves',
-		text_size: 	'5vw',
-		text_align: 'right',
-		image: 		'none',
+		text: 				'This is the time for the godesses to seize power.',
 		start_location: 	{
-							right: 	'15%',
-							top:    '30%',
-							left:   '-100%',
+							right: 	'10%',
+							bottom: '20%'
 		},
 		end_location: 		{
-							right: 	'20%',
-							top:    '30%'
+							right: 	'15%',
 		},
 	},
 	9:{
-		timeout: 	        15000,
-		time_on_screen:     2000,
-		text: 		'breaking apart into factions',
-		text_size: 	'5vw',
-		text_align: 'left',
-		image: 		'none',
-		start_location: 	{
-							left: 	'20%',
-							top:    '50%',
-							right:   '-100%',
-		},
-		end_location: 		{
-							left: 	'25%',
-							top:    '50%'
-		},
+		time_on_screen: 	4000,
+		image: 				'nun',
+		image_h: 			'move_left',
+		image_v: 			'move_up',
 	},
 	10:{
-		timeout: 			18000,
-		time_on_screen: 	3500,
-		image: 				'images/cards/farewell-587277_1280.jpg',
-		opacity: 			0.3,
+		text: 				'Blessing their followers, they start the war anew.',
 		start_location: 	{
-							right: 	'-0%',
-							top: 	'-50%',
-							left: 	'-0%',
-							bottom: '-0%',
+							left: 	'10%',
+							bottom: '10%'
 		},
 		end_location: 		{
-							right: 	'-20%',
-							top: 	'-60%',
-							left: 	'-0%',
-							bottom: '-0%',
+							left: 	'15%',
 		},
 	},
 	11:{
-		timeout: 	        18000,
-		time_on_screen:     2500,
-		text: 		'Some kept the old ways',
-		text_size: 	'5vw',
-		text_align: 'left',
-		image: 		'none',
+		time_on_screen: 	4000,
+		image: 				'recruitment_angel',
+		image_v: 			'move_down',
+	},
+	12:{
+		text: 				'The men gone, they recruit those that are left.',
 		start_location: 	{
 							left: 	'10%',
-							top:    '70%',
-							right:   '-100%',
+							top: 	'10%'
 		},
 		end_location: 		{
 							left: 	'15%',
-		},
-	},
-	12:{
-		timeout: 			21000,
-		time_on_screen: 	3500,
-		image: 				'images/cards/fantasy-2961723_640.jpg',
-		opacity: 			0.3,
-		start_location: 	{
-							right: 	'-0%',
-							top: 	'-0%',
-							left: 	'-0%',
-							bottom: '-80%',
-		},
-		end_location: 		{
-							right: 	'-10%',
-							top: 	'-0%',
-							left: 	'-10%',
-							bottom: '-110%',
 		},
 	},
 	13:{
-		timeout: 	        21000,
-		time_on_screen:     2500,
-		text: 		'Some embraced nature',
-		text_size: 	'5vw',
-		text_align: 'right',
-		image: 		'none',
+		time_on_screen: 	3000,
+		image: 				'battle_warder',
+		image_h: 			'move_left',
+	},
+	14:{
+		text: 				'Many great warriors arise to fight for their goddess.',
 		start_location: 	{
 							right: 	'10%',
-							top:    '10%',
-							left:   '-100%',
+							top: '40%'
 		},
 		end_location: 		{
 							right: 	'15%',
-		},
-	},
-	14:{
-		timeout: 			24000,
-		time_on_screen: 	3500,
-		image: 				'images/cards/fantasy-1390177_1280.jpg',
-		opacity: 			0.3,
-		start_location: 	{
-							right: 	'-0%',
-							top: 	'-0%',
-							left: 	'-0%',
-							bottom: '-60%',
-		},
-		end_location: 		{
-							right: 	'-10%',
-							top: 	'-0%',
-							left: 	'-0%',
-							bottom: '-90%',
 		},
 	},
 	15:{
-		timeout: 	        24000,
-		time_on_screen:     2500,
-		text: 		'Some sought magic in the north',
-		text_size: 	'5vw',
-		text_align: 'left',
-		image: 		'none',
-		start_location: 	{
-							left: 	'10%',
-							bottom:  '10%',
-							right:   '-100%',
-		},
-		end_location: 		{
-							left: 	'15%',
-		},
+		time_on_screen: 	3000,
+		image: 				'raise_dead',
+		image_h: 			'move_right',
+		image_v: 			'move_down',
 	},
 	16:{
-		timeout: 			27000,
-		time_on_screen: 	3500,
-		image: 				'images/cards/castle-832543_640.jpg',
-		opacity: 			0.3,
+		text: 				'The slain are raised...',
 		start_location: 	{
-							right: 	'-10%',
-							top: 	'-0%',
-							left: 	'-10%',
-							bottom: '-20%',
+							right: 	'10%',
+							top: '60%'
 		},
 		end_location: 		{
-							right: 	'-0%',
-							top: 	'-20%',
-							left: 	'-0%',
-							bottom: '-0%',
+							right: 	'15%',
 		},
 	},
 	17:{
-		timeout: 	        27000,
-		time_on_screen:     2500,
-		text: 		'Some found a god to worship',
-		text_size: 	'5vw',
-		text_align: 'right',
-		image: 		'none',
-		start_location: 	{
-							right: 	'10%',
-							top:    '10%',
-							left:   '-100%',
-		},
-		end_location: 		{
-							right: 	'15%',
-		},
+		time_on_screen: 	3000,
+		image: 				'burning_imp',
+		image_h: 			'move_left',
 	},
 	18:{
-		timeout: 			30000,
-		time_on_screen: 	3500,
-		image: 				'images/cards/monster-5369480_640.jpg',
-		opacity: 			0.3,
+		text: 				'Daemons are summoned...',
 		start_location: 	{
-							right: 	'-0%',
-							top: 	'-0%',
-							left: 	'-20%',
-							bottom: '-200%',
+							left: 	'10%',
+							top: '40%'
 		},
 		end_location: 		{
-							right: 	'-0%',
-							top: 	'-0%',
-							left: 	'-10%',
-							bottom: '-150%',
+							left: 	'15%',
 		},
 	},
 	19:{
-		timeout: 	        30000,
-		time_on_screen:     2500,
-		text: 		'Others embraced darkness',
-		text_size: 	'5vw',
-		text_align: 'left',
-		image: 		'none',
-		start_location: 	{
-							left: 	'10%',
-							bottom:  '10%',
-							right:   '-100%',
-		},
-		end_location: 		{
-							left: 	'15%',
-		},
+		time_on_screen: 	6000,
+		image: 				'barbarian_elite',
+		image_h: 			'move_left',
+		image_v: 			'move_up',
 	},
 	20:{
-		timeout: 			33000,
-		time_on_screen: 	3500,
-		image: 				'images/cards/fire-2648873_640.jpg',
-		opacity: 			0.3,
-		start_location: 	{
-							right: 	'-0%',
-							top: 	'-0%',
-							left: 	'-0%',
-							bottom: '-60%',
-		},
-		end_location: 		{
-							right: 	'-10%',
-							top: 	'-30%',
-							left: 	'-10%',
-							bottom: '-30%',
-		},
-	},
-	21:{
-		timeout: 	        33000,
-		time_on_screen:     2500,
-		text: 		'or just wanted the world to burn',
-		text_size: 	'5vw',
-		text_align: 'left',
-		image: 		'none',
-		start_location: 	{
-							left: 	'10%',
-							top:  	'10%',
-							right:   '-100%',
-		},
-		end_location: 		{
-							left: 	'15%',
-		},
-	},
-	22:{
-		timeout: 	        37000,
-		time_on_screen:     2500,
-		text: 		'After many years of war...',
-		text_size: 	'5vw',
-		text_align: 'left',
-		image: 		'none',
-		start_location: 	{
-							left: 	'10%',
-							bottom:  '10%',
-							right:   '-100%',
-		},
-		end_location: 		{
-							left: 	'15%',
-		},
-	},
-	23:{
-		timeout: 			39000,
-		time_on_screen: 	8000,
-		image: 				'images/cards/take-532097_640.jpg',
-		opacity: 			0.3,
-		start_location: 	{
-							right: 	'-0%',
-							top: 	'-0%',
-							left: 	'-0%',
-							bottom: '-60%',
-		},
-		end_location: 		{
-							right: 	'-10%',
-							top: 	'-30%',
-							left: 	'-10%',
-							bottom: '-30%',
-		},
-	},
-	24:{
-		timeout: 	        40000,
-		time_on_screen:     2500,
-		text: 		'a hero dreamt of a book',
-		text_size: 	'5vw',
-		text_align: 'left',
-		image: 		'none',
-		start_location: 	{
-							left: 	'10%',
-							top:  	'10%',
-							right:   '-100%',
-		},
-		end_location: 		{
-							left: 	'15%',
-		},
-	},
-	25:{
-		timeout: 	        42000,
-		time_on_screen:     2500,
-		text: 		'that would let him bring man together',
-		text_size: 	'5vw',
-		text_align: 'left',
-		image: 		'none',
-		start_location: 	{
-							left: 	'10%',
-							top:  	'40%',
-							right:   '-100%',
-		},
-		end_location: 		{
-							left: 	'15%',
-		},
-	},
-	26:{
-		timeout: 	        45000,
-		time_on_screen:     2500,
-		text: 		'The question is...',
-		text_size: 	'5vw',
-		text_align: 'right',
-		image: 		'none',
+		text: 				'It is time for war!',
 		start_location: 	{
 							right: 	'10%',
-							top:  	'10%',
-							left:   '-100%',
+							bottom: '10%'
 		},
 		end_location: 		{
 							right: 	'15%',
 		},
 	},
-	27:{
-		timeout: 	        48000,
-		time_on_screen:     4000,
-		text: 		'Will you?',
-		text_size: 	'5vw',
-		text_align: 'center',
-		image: 		'none',
-		start_location: 	{
-							right: 	'0%',
-							top:  	'40%',
-							left:   '0%',
-		},
-		end_location: 		{
-		},
+	21:{
+		time_on_screen: 	3000,
+		text: 				'',
 	},
 }
 
+var transition_period = 1000;
+var step_timeout = 0;
+var image_time_on_screen = 0;
+eachoa(intro_steps, function(step_number, step_info){
+	if(step_info['timeout'] == undefined){step_info['timeout'] = step_timeout;}
+	if(step_info['time_on_screen'] == undefined){step_info['time_on_screen'] = image_time_on_screen - transition_period;}
+	if(step_info['image'] != undefined && all_available_cards[step_info['image']] != undefined)
+	{
+		step_info['image'] = all_available_cards[step_info['image']]['image'];
+	}
+	else
+	{
+		step_info['image'] = 'none';
+	}
+	if(step_info['start_location'] == undefined){step_info['start_location'] = {};}
+	if(step_info['end_location'] == undefined){step_info['end_location'] = {};}
+	if(step_info['image'] != 'none')
+	{
+		step_info['opacity'] = 0.3;
+		if(step_info['image_h'] == 'move_left')
+		{
+			step_info['start_location']['right'] = '-10%';
+			step_info['end_location']['left'] = '-10%';
+		}
+		if(step_info['image_h'] == 'move_right')
+		{
+			step_info['start_location']['left'] = '-10%';
+			step_info['end_location']['right'] = '-10%';
+		}
+		if(step_info['image_v'] == 'move_up')
+		{
+			step_info['start_location']['bottom'] = '-10%';
+			step_info['end_location']['top'] = '-10%';
+		}
+		if(step_info['image_v'] == 'move_down')
+		{
+			step_info['start_location']['top'] = '-10%';
+			step_info['end_location']['bottom'] = '-10%';
+		}
+		if(step_info['start_location']['left'] == undefined){step_info['start_location']['left'] = '0%';}
+		if(step_info['start_location']['right'] == undefined){step_info['start_location']['right'] = '0%';}
+		if(step_info['start_location']['top'] == undefined){step_info['start_location']['top'] = '0%';}
+		if(step_info['start_location']['bottom'] == undefined){step_info['start_location']['bottom'] = '0%';}
+		if(step_info['end_location']['left'] == undefined){step_info['end_location']['left'] = '0%';}
+		if(step_info['end_location']['right'] == undefined){step_info['end_location']['right'] = '0%';}
+		if(step_info['end_location']['top'] == undefined){step_info['end_location']['top'] = '0%';}
+		if(step_info['end_location']['bottom'] == undefined){step_info['end_location']['bottom'] = '0%';}	
+		if(step_info['time_on_screen'] != undefined)
+		{
+			image_time_on_screen = step_info['time_on_screen'];
+		}
+	}
+	else
+	{
+		if(step_info['time_on_screen'] != undefined)
+		{
+			//if(step_timeout == 0){step_timeout -= 1000;}
+			step_timeout += step_info['time_on_screen'] + transition_period;
+			
+		}
+	}
+});
