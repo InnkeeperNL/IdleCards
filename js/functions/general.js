020047a41e9826422375a96fa4957047ab97e80f
InnkeeperNL <hotfmn@gmail.com>
2026-02-08 08:27:32 +0100

    Update general.js

--- js/functions/general.js
+++ js/functions/general.js
@@ -382,3 +382,12 @@ function getCurrentYear()
   var yearnumber =  getWeekNumber(new Date());
   return yearnumber[0];
 }
+
+function getHash(input){
+  var hash = 0, len = input.length;
+  for (var i = 0; i < len; i++) {
+    hash  = ((hash << 5) - hash) + input.charCodeAt(i);
+    hash |= 0; // to 32bit integer
+  }
+  return hash;
+}
